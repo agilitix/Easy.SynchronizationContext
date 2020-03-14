8224e06dbdf645c8efab7352f076ae1ff10b075b
agilitix <adsbox@outlook.fr>
2020-03-14 17:52:33 +0100

    Added wpf app sample with dispatcher

---
+++ Wpf/WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+using System.Windows;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private IUiDispatcher _dispacther;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _dispacther = new UiDispatcher();
+        }
+
+        private void ShowTime()
+        {
+            int thId = Thread.CurrentThread.ManagedThreadId;
+
+            for (int i = 0; i < 20; ++i)
+            {
+                // Update a text box in the UI thread.
+                _dispacther.RunSync(() => { textBox.Text = DateTime.Now.ToString($"{thId} - MM/dd/yyyy HH:mm:ss"); });
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void Button_ShowTime(object sender, RoutedEventArgs e)
+        {
+            // Start new thread to show how to update the UI from a worker thread.
+            Thread th = new Thread(ShowTime);
+            th.IsBackground = true; // The thread terminates immediately if the app/UI is closed.
+            th.Start();
+        }
+
+        private void Button_NewWindow(object sender, RoutedEventArgs e)
+        {
+            Thread windowThread = new Thread(() =>
+                                             {
+                                                 Window w = new MainWindow();
+                                                 w.Show();
+                                                 w.Closed += (sender2, e2) => w.Dispatcher.InvokeShutdown();
+
+                                                 // Start pumping messages.
+                                                 System.Windows.Threading.Dispatcher.Run();
+                                             });
+
+            // Start new UI thread with its own message pump.
+            windowThread.SetApartmentState(ApartmentState.STA);
+            windowThread.IsBackground = true;
+            windowThread.Start();
+        }
+    }
+}
---
+++ Wpf/WpfApp/UiDispatcher.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Threading;
+
+namespace WpfApp
+{
+    public class UiDispatcher : IUiDispatcher
+    {
+        private readonly Dispatcher _dispatcher;
+
+        public UiDispatcher()
+        {
+            _dispatcher = Dispatcher.FromThread(Thread.CurrentThread);
+        }
+
+        public void RunSync(Action action)
+        {
+            if (CheckAccess())
+            {
+                action();
+            }
+            else
+            {
+                _dispatcher.Invoke(action, new object[0]);
+            }
+        }
+
+        public void RunAsync(Action action)
+        {
+            if (CheckAccess())
+            {
+                action();
+            }
+            else
+            {
+                _dispatcher.BeginInvoke(action, new object[0]);
+            }
+        }
+
+        public bool CheckAccess()
+        {
+            return _dispatcher.CheckAccess();
+        }
+    }
+}
